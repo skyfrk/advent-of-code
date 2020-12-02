e0b82920b24ffbf4f26388cf0ba59d2e79b05a87
skyfrk <github@esslinger.dev>
2020-12-02 17:35:45 +0100

    feat(2020): implement day 2

---
+++ 2020/AdventOfCode.2020.Day02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var regex = new Regex(@"(\d+)+-(\d+)+ (\w){1}: (\w+)");
+
+var validPart1PasswordCount = 0;
+var validPart2PasswordCount = 0;
+
+foreach (var line in input)
+{
+    var groups = regex.Match(line).Groups;
+    var min = int.Parse(groups[1].Value);
+    var max = int.Parse(groups[2].Value);
+    var letter = groups[3].Value[0];
+    var password = groups[4].Value;
+    var appearanceCount = password.Count(pwLetter => pwLetter == letter);
+
+    if (appearanceCount >= min && appearanceCount <= max) validPart1PasswordCount++;
+
+    if (max <= password.Length && min <= max && (password[min - 1] == letter ^ password[max - 1] == letter)) validPart2PasswordCount++;
+}
+
+Console.WriteLine($" {nameof(validPart1PasswordCount)} : {validPart1PasswordCount}");
+Console.WriteLine($" {nameof(validPart2PasswordCount)} : {validPart2PasswordCount}");
