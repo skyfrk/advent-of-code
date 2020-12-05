7a71f86f186ae5bbcf6afddb8f896ba0df2c16c2
github@esslinger.dev <simon@esslinger.dev>
2020-12-05 12:55:32 +0100

    feat(2020): implement day 5

---
+++ 2020/AdventOfCode.2020.Day05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt");
+
+var seats = new List<(int row, int column)>();
+
+foreach (var line in input)
+{
+    var upperRow = 127;
+    var lowerRow = 0;
+
+    for (int i = 0; i < 7; i++)
+    {
+        double diff = upperRow - lowerRow;
+        if (line[i] == 'F') upperRow -= (int)Math.Ceiling(diff / 2);
+        if (line[i] == 'B') lowerRow += (int)Math.Ceiling(diff / 2);
+    }
+
+    var upperColumn = 7;
+    var lowerColumn = 0;
+
+    for (int i = 7; i < 10; i++)
+    {
+        double diff = upperColumn - lowerColumn;
+        if (line[i] == 'L') upperColumn -= (int)Math.Ceiling(diff / 2);
+        if (line[i] == 'R') lowerColumn += (int)Math.Ceiling(diff / 2);
+    }
+
+    var seatId = upperRow * 8 + upperColumn;
+
+    seats.Add((upperRow, upperColumn));
+}
+
+var maxSeatId = seats.Select(seat => seat.row * 8 + seat.column).Max();
+
+Console.WriteLine($"Part 1: {maxSeatId}");
+
+var takenSeats = seats
+    .Where(seat => seat.row != 0 && seat.row != 127)
+    .Select(seat => seat.row * 8 + seat.column);
+
+var mySeatId = Enumerable
+    .Range(takenSeats.Min(), takenSeats.Count() + 1)
+    .Except(takenSeats)
+    .First();
+
+Console.WriteLine($"Part 2: {mySeatId}");
---
+++ 2020/AdventOfCode.2020.Day05/README.md
@@ -0,0 +1,3 @@
+﻿# Day 5
+
+https://adventofcode.com/2020/day/5
