256083ae1eca2f04c30faa5193bf0e7a027678ea
skyfrk <github@esslinger.dev>
2020-12-08 20:17:52 +0100

    chore(2020): prepare day 09

---
+++ src/AdventOfCode.2020.Day09/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
---
+++ src/AdventOfCode.2020.Day09/README.md
@@ -0,0 +1,10 @@
+﻿# Day 9
+
+https://adventofcode.com/2020/day/9
+
+- Part 1:
+- Part 2:
+
+## Part 1
+
+## Part 2
