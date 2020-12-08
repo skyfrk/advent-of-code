8257b047906e38c39ef4dc8cfe3b6f12bac80811
skyfrk <github@esslinger.dev>
2020-12-08 13:54:48 +0100

    feat(2020): implement day 8 part 1

--- src/AdventOfCode.2020.Day08/Program.cs
+++ src/AdventOfCode.2020.Day08/Program.cs
@@ -1,7 +1,41 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-var input = File.ReadAllText("input.txt");
+var input = File.ReadAllLines("input.txt");
 
-var lines = input.Split("\r\n\r\n");
+var instructionRegex = new Regex(@"^(nop|acc|jmp){1} ([\+|-]{1}\d+)$");
+
+var instructions = input
+    .Select(line =>
+    {
+        var match = instructionRegex.Match(line);
+        var operation = match.Groups[1].Value;
+        var value = int.Parse(match.Groups[2].Value);
+        return (Operation: operation, Value: value, ExecutedCount: 0);
+    })
+    .ToArray();
+
+var accumulator = 0;
+
+for (int i = 0; i < instructions.Length; i++)
+{
+    var (operation, value, executedCount) = instructions[i];
+
+    if (executedCount > 0) break;
+
+    instructions[i].ExecutedCount++;
+
+    if (operation == "jmp")
+    {
+        i += value - 1;
+    }
+
+    if (operation == "acc")
+    {
+        accumulator += value;
+    }
+}
+
+Console.WriteLine($"Part 1: {accumulator}");
