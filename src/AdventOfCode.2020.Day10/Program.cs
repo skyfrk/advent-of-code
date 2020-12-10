7f0e2148bbe8614ddda2f63522870c003a1d69cc
skyfrk <github@esslinger.dev>
2020-12-10 12:26:18 +0100

    feat(2020): implement day 10 part 1

--- src/AdventOfCode.2020.Day10/Program.cs
+++ src/AdventOfCode.2020.Day10/Program.cs
@@ -1,5 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input.txt").Select(uint.Parse);
+
+void SolvePart1()
+{
+    var deviceJoltage = input.Max() + 3;
+    var allDevices = input.OrderBy(d => d).Append(deviceJoltage);
+
+    uint prev = 0;
+    List<uint> diffs = new();
+    foreach (var device in allDevices)
+    {
+        uint diff = device - prev;
+        prev = device;
+        if (diff > 3) throw new InvalidOperationException();
+        diffs.Add(diff);
+    }
+
+    var oneDiffCount = diffs.Where(d => d == 1).Count();
+    var threeDiffCount = diffs.Where(d => d == 3).Count();
+
+    Console.WriteLine($"Part 1: {oneDiffCount * threeDiffCount}");
+}
+
+SolvePart1();
