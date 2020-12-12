65cf91b1dead690f5d4f3137c4466d7130516d6b
github@esslinger.dev <simon@esslinger.dev>
2020-12-12 17:20:51 +0100

    feat(2020): implement day 12 part 2

--- src/AdventOfCode.2020.Day12/Program.cs
+++ src/AdventOfCode.2020.Day12/Program.cs
@@ -1,72 +1,133 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 
 var input = File.ReadAllLines("input.txt").Select(line => (action: line[0], value: int.Parse(line[1..])));
 
-uint orientation = 90; // east
-int posNorthSouth = 0;
-int posEastWest = 0;
-
-foreach (var (action, value) in input)
+void SolvePart1()
 {
-    Console.WriteLine($"Orientation: {orientation} NorthSouth: {posNorthSouth} EastWest: {posEastWest} Action: {action}{value}");
+    uint orientation = 90; // east
+    int posNorthSouth = 0;
+    int posEastWest = 0;
 
-    if (action == 'N')
+    foreach (var (action, value) in input)
     {
-        posNorthSouth += value;
-        continue;
+        if (action == 'N')
+        {
+            posNorthSouth += value;
+            continue;
+        }
+
+        if (action == 'S')
+        {
+            posNorthSouth -= value;
+            continue;
+        }
+
+        if (action == 'E')
+        {
+            posEastWest += value;
+            continue;
+        }
+
+        if (action == 'W')
+        {
+            posEastWest -= value;
+            continue;
+        }
+
+        if (action == 'R' || action == 'L')
+        {
+            orientation = GetOrientation(orientation, value, action);
+            continue;
+        }
+
+        if (action == 'F')
+        {
+            if (orientation == 0) posNorthSouth += value;
+            if (orientation == 90) posEastWest += value;
+            if (orientation == 180) posNorthSouth -= value;
+            if (orientation == 270) posEastWest -= value;
+        }
     }
 
-    if (action == 'S')
-    {
-        posNorthSouth -= value;
-        continue;
-    }
+    Console.WriteLine($"Part 1: {Math.Abs(posNorthSouth) + Math.Abs(posEastWest)}");
 
-    if (action == 'E')
+    static uint GetOrientation(uint currentOrientation, int turnValue, char turnDirection)
     {
-        posEastWest += value;
-        continue;
-    }
+        if (turnValue == 0) return currentOrientation;
 
-    if (action == 'W')
-    {
-        posEastWest -= value;
-        continue;
-    }
-
-    if (action == 'R' || action == 'L')
-    {
-        // Console.WriteLine($"Orientation before: {orientation} Action: {action}{value}");
-        orientation = GetOrientation(orientation, value, action);
-        // Console.WriteLine($"Orientation after: {orientation}");
-        continue;
-    }
-
-    if (action == 'F')
-    {
-        if (orientation == 0) posNorthSouth += value;
-        if (orientation == 90) posEastWest += value;
-        if (orientation == 180) posNorthSouth -= value;
-        if (orientation == 270) posEastWest -= value;
+        if (turnDirection == 'R')
+        {
+            return (currentOrientation + (uint)turnValue) % 360;
+        }
+        else
+        {
+            return (360 + (currentOrientation - (uint)turnValue)) % 360;
+        }
     }
 }
 
-Console.WriteLine($"Orientation: {orientation} NorthSouth: {posNorthSouth} EastWest: {posEastWest}");
-
-Console.WriteLine($"Part 1: {Math.Abs(posNorthSouth) + Math.Abs(posEastWest)}");
-
-static uint GetOrientation(uint currentOrientation, int turnValue, char turnDirection)
+void SolvePart2()
 {
-    if (turnValue == 0) return currentOrientation;
+    // x-axis: east west
+    // y-axis: north south
+    var waypointPosition = (x: 10, y: 1);
+    var shipPosition = (x: 0, y: 0);
 
-    if (turnDirection == 'R')
+    foreach (var (action, value) in input)
     {
-        return (currentOrientation + (uint)turnValue) % 360;
+        switch (action)
+        {
+            case 'N':
+                waypointPosition.y += value;
+                break;
+            case 'S':
+                waypointPosition.y -= value;
+                break;
+            case 'E':
+                waypointPosition.x += value;
+                break;
+            case 'W':
+                waypointPosition.x -= value;
+                break;
+            case 'R':
+                RotateWaypoint(value);
+                break;
+            case 'L':
+                RotateWaypoint(-value);
+                break;
+            case 'F':
+                shipPosition.x += value * waypointPosition.x;
+                shipPosition.y += value * waypointPosition.y;
+                break;
+            default:
+                throw new InvalidOperationException();
+        };
     }
-    else
+
+    Console.WriteLine($"Part 2: {Math.Abs(shipPosition.x) + Math.Abs(shipPosition.y)}");
+
+    void RotateWaypoint(int value)
     {
-        return (360 + (currentOrientation - (uint)turnValue)) % 360;
+        for (int i = 0; i < Math.Abs(value / 90); i++)
+        {
+            if (value < 0)
+            {
+                var oldX = waypointPosition.x;
+                waypointPosition.x = -waypointPosition.y;
+                waypointPosition.y = oldX;
+            } 
+            else
+            {
+                var oldX = waypointPosition.x;
+                waypointPosition.x = waypointPosition.y;
+                waypointPosition.y = -oldX;
+            }
+        }
     }
 }
+
+SolvePart1();
+SolvePart2();
