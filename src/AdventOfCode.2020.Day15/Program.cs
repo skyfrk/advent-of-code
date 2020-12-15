118784608f1480c9ee5999cba4e94a221f655133
github@esslinger.dev <simon@esslinger.dev>
2020-12-15 13:09:11 +0100

    feat(2020): implement day 15 part 1

--- src/AdventOfCode.2020.Day15/Program.cs
+++ src/AdventOfCode.2020.Day15/Program.cs
@@ -2,4 +2,25 @@
 using System.IO;
 using System.Linq;
 
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input.txt")[0].Split(',').Select(int.Parse).ToArray();
+
+var targetNumber = 2020;
+var spokenNumbers = input.ToList();
+
+for(int i = input.Length; i < targetNumber; i++)
+{
+    var prevNum = spokenNumbers[i - 1];
+
+    var lastIdx = spokenNumbers.SkipLast(1).ToList().LastIndexOf(prevNum);
+
+    if(lastIdx == -1)
+    {
+        spokenNumbers.Add(0);
+    }
+    else
+    {
+        spokenNumbers.Add(i - 1 - lastIdx);
+    }
+}
+
+Console.WriteLine($"Part 1: {spokenNumbers[targetNumber - 1]}");
