1f9777a6c0ee6d12ba9cba9b44a8849d24165691
github@esslinger.dev <simon@esslinger.dev>
2020-12-26 02:21:47 +0100

    feat(2020): implement day 25 part 1

--- src/AdventOfCode.2020.Day25/Program.cs
+++ src/AdventOfCode.2020.Day25/Program.cs
@@ -2,4 +2,47 @@
 using System.IO;
 using System.Linq;
 
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input.txt").Select(long.Parse).ToArray();
+
+long cardPublicKey = input[0];
+long doorPublicKey = input[1];
+
+const long initialSubjectValue = 7;
+
+var cardLoopSize = GetLoopSize(cardPublicKey, initialSubjectValue);
+var doorLoopSize = GetLoopSize(doorPublicKey, initialSubjectValue);
+
+var encryptionKeyDoor = Transform(doorLoopSize, cardPublicKey); 
+var encryptionKeyCard = Transform(cardLoopSize, doorPublicKey);
+
+if(encryptionKeyDoor != encryptionKeyCard) throw new InvalidOperationException();
+
+Console.WriteLine($"Part 1: {encryptionKeyCard}");
+
+long Transform(long loopSize, long subject)
+{
+    long value = 1;
+
+    for(long i = 0; i < loopSize; i++)
+    {
+        value *= subject;
+        value %= 20201227;
+    }
+
+    return value;
+}
+
+long GetLoopSize(long publicKey, long subject)
+{
+	long value = 1;
+
+	int loopSize = 0;
+
+	while (value != publicKey)
+	{
+		value *= subject;
+		value %= 20201227;
+		loopSize++;
+	}
+	return loopSize;
+}
